bb31a3bd5880cdbf7b131e48e32c1a25782e6e4f
BAS6275 <bas6275@gmail.com>
2020-07-27 19:02:57 +0700

    init Project Shopping

---
+++ layouts/default.vue
@@ -0,0 +1,178 @@
+<template>
+  <v-app style="background-color: #fff;">
+    <div>
+      <v-navigation-drawer v-model="toggleMenu" temporary absolute>
+        <v-list>
+          <v-list-item
+            v-for="btn in menuList"
+            :key="btn.id"
+            @click="toggleMenu = false"
+          >
+            {{ btn.name }}
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar color="orange darken-4" dense sticky app>
+        <v-icon large color="white">
+          mdi-shield-home
+        </v-icon>
+        <v-app-bar-nav-icon
+          class="d-flex d-sm-none"
+          @click="toggleMenu = !toggleMenu"
+        ></v-app-bar-nav-icon>
+
+        <div class="d-none d-sm-flex">
+          <v-btn v-for="btn in menuList" :key="btn.id" text>{{
+            btn.name
+          }}</v-btn>
+        </div>
+
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-basket</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+
+      <v-main>
+        <nuxt />
+      </v-main>
+
+      <v-footer color="orange darken-4" padless>
+        <v-row justify="center" no-gutters>
+          <v-col cols="12" sm="3" class="white--text benefit-link"
+            ><h4>รวมลิงค์ที่เป็นประโยชน์</h4>
+            <a>แคตตาล็อกและโบรชัวร์</a><br />
+            <a>แอพพลิเคชั่นอิเกีย</a><br />
+            <a>โปรแกรมออกแบบของอิเกีย</a><br />
+            <a>การรับประกันสินค้า</a><br />
+            <a>สโตร์อิเกียใกล้บ้านคุณ</a><br />
+            <a>ร้านอาหารอิเกีย</a><br />
+            <a>มุมอาหารและขนมสวีเดน</a><br />
+            <a>IKEA Family</a><br />
+            <a>IKEA Business</a><br
+          /></v-col>
+          <v-col cols="12" sm="3" class="white--text benefit-link"
+            ><h4>รวมลิงค์ที่เป็นประโยชน์</h4>
+            <a>แคตตาล็อกและโบรชัวร์</a><br />
+            <a>แอพพลิเคชั่นอิเกีย</a><br />
+            <a>โปรแกรมออกแบบของอิเกีย</a><br />
+            <a>การรับประกันสินค้า</a><br />
+            <a>สโตร์อิเกียใกล้บ้านคุณ</a><br />
+            <a>ร้านอาหารอิเกีย</a><br />
+            <a>มุมอาหารและขนมสวีเดน</a><br />
+            <a>IKEA Family</a><br />
+            <a>IKEA Business</a><br
+          /></v-col>
+          <v-col cols="12" sm="3" class="white--text benefit-link"
+            ><h4>รวมลิงค์ที่เป็นประโยชน์</h4>
+            <a>แคตตาล็อกและโบรชัวร์</a><br />
+            <a>แอพพลิเคชั่นอิเกีย</a><br />
+            <a>โปรแกรมออกแบบของอิเกีย</a><br />
+            <a>การรับประกันสินค้า</a><br />
+            <a>สโตร์อิเกียใกล้บ้านคุณ</a><br />
+            <a>ร้านอาหารอิเกีย</a><br />
+            <a>มุมอาหารและขนมสวีเดน</a><br />
+            <a>IKEA Family</a><br />
+            <a>IKEA Business</a><br
+          /></v-col>
+          <v-col cols="12" sm="3" class="white--text benefit-link"
+            ><h4>รวมลิงค์ที่เป็นประโยชน์</h4>
+            <a>แคตตาล็อกและโบรชัวร์</a><br />
+            <a>แอพพลิเคชั่นอิเกีย</a><br />
+            <a>โปรแกรมออกแบบของอิเกีย</a><br />
+            <a>การรับประกันสินค้า</a><br />
+            <a>สโตร์อิเกียใกล้บ้านคุณ</a><br />
+            <a>ร้านอาหารอิเกีย</a><br />
+            <a>มุมอาหารและขนมสวีเดน</a><br />
+            <a>IKEA Family</a><br />
+            <a>IKEA Business</a><br
+          /></v-col>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dropdown_font: ['Arial', 'Calibri', 'Courier', 'Verdana'],
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+      toggleMenu: false,
+      menuList: [
+        {
+          id: 1,
+          name: 'btn1',
+        },
+        {
+          id: 2,
+          name: 'btn2',
+        },
+        {
+          id: 3,
+          name: 'btn3',
+        },
+        {
+          id: 4,
+          name: 'btn4',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.mr-top-2 {
+  margin: 2em;
+}
+.bg-white {
+  background-color: white;
+}
+.benefit-link > a {
+  color: white !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,230 @@
+<template>
+  <div>
+    <div max-width="auto" class="mr-top-2">
+      <v-carousel height="300" show-arrows-on-hover cycle>
+        <v-carousel-item
+          v-for="(item, i) in imgItems"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
+    </div>
+
+    <!-- Product image -->
+    <v-row class="d-flex flex-wrap mr-top-2 justify-center">
+      <v-col
+        v-for="(item, index) in productList"
+        :key="index"
+        cols="12"
+        sm="4"
+        md="3"
+      >
+        <v-hover v-slot:default="{ hover }">
+          <v-card :elevation="hover ? 16 : 2" class="mr-top-2">
+            <!-- มีปัญหา -->
+            <v-img class="setPosition" :src="item.src"></v-img>
+
+            <v-card-title>{{ item.productName }}</v-card-title>
+            <v-card-text>
+              <div>
+                100$
+              </div>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="dark--text ml-4">4.5 (413)</div>
+              </v-row>
+
+              <div>
+                {{ item.detiel }}
+              </div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-actions>
+              <v-btn color="deep-purple lighten-2" text>
+                หยิบเข้าตระกร้า
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+
+    <!-- end of picture  -->
+    <div class="text-center">
+      <v-btn
+        class="ma-2"
+        tile
+        outlined
+        color="success"
+        @click="seeMoreHandler()"
+        >สินค้าเพิ่มเติม</v-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+      items: [
+        {
+          id: 1,
+          productName: 'chair',
+          detiel: 'รายละเอียดสินค้า',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQSPAYBDq_ogHF22hywk1jNI_TS_EgpYhMA6w&usqp=CAU',
+        },
+        {
+          id: 2,
+          productName: 'chair2',
+          detiel: 'รายละเอียดสินค้า2',
+          src: 'https://www.mangosiam.com/images/slide/2.jpg',
+        },
+        {
+          id: 3,
+          productName: 'chair3',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 4,
+          productName: 'chair4',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 5,
+          productName: 'chair5',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 6,
+          productName: 'chair6',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 7,
+          productName: 'chair7',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 8,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 9,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 10,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 11,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 12,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 13,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 14,
+          productName: 'chair8',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 15,
+          productName: 'chair15',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 16,
+          productName: 'chair16',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 17,
+          productName: 'chair16',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+        {
+          id: 18,
+          productName: 'chair18',
+          detiel: 'รายละเอียดสินค้า3',
+        },
+      ],
+      imgItems: [
+        {
+          src:
+            'https://family.ikea.co.th/storage/sale-preview/2020-JUL/sale-page-banner-th.jpg',
+        },
+        {
+          src:
+            'https://www.whatphone.net/wp-content/uploads/2019/05/promotion-banana-tme-2019-may.jpg',
+        },
+        {
+          src:
+            'https://d2rmw7fiarhdss.cloudfront.net/media//saleandpromotion/category/New-normal-promotion-page-1000x495.png',
+        },
+        {
+          src:
+            'https://www.sansiri.com/singlehouse/kanasiri-wongwaen-lamlukka/img/Coverbanner/kanasiri-wongwaen-lamlukka-house-cover-banner-desktop-1920x800-02.jpg?v=1.0.1',
+        },
+      ],
+      productList: [],
+      productLength: 8,
+    }
+  },
+  created() {
+    this.productList = this.items.slice(0, this.productLength)
+  },
+  methods: {
+    seeMoreHandler() {
+      this.productList = this.items.slice(
+        0,
+        this.productLength + this.productList.length
+      )
+    },
+  },
+}
+</script>
+//CSS stye
+<style>
+.bg-white {
+  background-color: white;
+}
+.mr-top-2 {
+  margin: 20px;
+}
+
+.mr-top-banner {
+  margin-top: 5px;
+}
+
+.setPosition {
+  max-width: 100%;
+  height: auto !important;
+}
+</style>
